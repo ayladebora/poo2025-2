e7f15511a06edb0bd382f67244ae803ce942a35d
ayla <ayla@dcx.ufpb.br>
2025-11-18 14:26:14 -0300

    Eliminando erros

---
+++ src/main/java/br/ufpb/dcx/amigosecreto/MensagemParaTodos.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.amigosecreto;
+
+public class MensagemParaTodos extends Mensagem {
+
+    public MensagemParaTodos(String texto, String emailRemetente, boolean anonima){
+        super(texto, emailRemetente, anonima);
+    }
+
+    public String getTextoCompletoAExibir(){
+        //TODO: IMPLEMENTAR
+        return null;
+    }
+}
--- src/main/java/br/ufpb/dcx/amigosecreto/SistemaAmigo.java
+++ src/main/java/br/ufpb/dcx/amigosecreto/SistemaAmigo.java
@@ -1,4 +1,29 @@
 package br.ufpb.dcx.amigosecreto;
 
+import java.util.List;
+
 public class SistemaAmigo {
+    private List<Amigo> amigos;
+    private List<Mensagem> mensagens;
+
+
+    public List<Mensagem> pesquisaMensagensAnonimas(){
+        //TODO: FAZER DEPOIS
+        return null;
+    }
+
+
+    public void configuraAmigoSecretoDe(String emailDaPessoa, String emailAmigoSorteado) throws AmigoInexistenteException{
+        //TODO: FAZER DEPOIS
+    }
+    public List<Mensagem> pesquisaTodasAsMensagens() {
+        return this.mensagens;
+    }
+    public String pesquisaAmigoSecretoDe(String emailDaPessoa) throws AmigoInexistenteException,
+            AmigoNaoSorteadoException{
+        //TODO: Fazer depois
+        return null;
+
+    }
+
 }
